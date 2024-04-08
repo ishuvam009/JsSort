b66c9520ca2143b8145d3afc8219ed32f9353ed4
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2024-04-08 18:46:25 +0530

    Initial Commit

---
+++ DAY 1/operators.js
@@ -0,0 +1,38 @@
+// JS Operators
+
+/*
+    Arithmetic Operators
+    Operator	Description
+            +	Addition
+            -	Subtraction
+            *	Multiplication
+            **	Exponentiation (ES2016)
+            /	Division
+            %	Modulus (Division Remainder)
+            ++	Increment
+            --	Decrement
+*/
+
+/*
+         Assignment Operators
+        Comparison Operators
+        String Operators
+        Logical Operators
+        Bitwise Operators
+        Ternary Operators
+        Type Operators
+*/
+
+
+// Prefix and postfix
+
+let a = 20;
+let b = a++; //As we are using the increment operator in postfix that's why the first 20 is assigned to b and then the value of a is incremented to ++ ie. 21
+console.log(`a:${a}, b:${b}`);
+
+console.log();
+console.log();
+
+let y = 40;
+let z = ++y;
+console.log(`y:${y}, z:${z}`);
