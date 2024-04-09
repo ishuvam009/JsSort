e7a740b5a6658f4e16d326cabe6440a3c1f903bb
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2024-04-09 20:24:27 +0530

    Initial Commit

---
+++ Day 2/array.js
@@ -0,0 +1,15 @@
+let usersArr = ['shuvam','biswajit','ajit','azad','abhijeet','poo'];
+
+console.log(usersArr);
+
+let newArray = usersArr;
+
+newArray[2] = 'Mohan';
+
+console.log(newArray);
+console.log(usersArr);
+
+// Array handbook
+
+// Array:   push(), pop(), shift(), unshift(), splice(), slice(),
+// concat(), forEach(), map(), filter(), reduce(), find(), sort()
