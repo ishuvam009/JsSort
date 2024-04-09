b2d18b5dc3e6d9c7c85a5d4c3498f263b0180a18
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2024-04-09 22:57:09 +0530

    Initial Commit

--- Day 2/array.js
+++ Day 2/array.js
@@ -1,15 +1,22 @@
-let usersArr = ['shuvam','biswajit','ajit','azad','abhijeet','poo'];
-
-console.log(usersArr);
-
-let newArray = usersArr;
-
-newArray[2] = 'Mohan';
-
-console.log(newArray);
-console.log(usersArr);
+// let usersArr = ['shuvam','biswajit','ajit','azad','abhijeet','poo'];
+//
+// console.log(usersArr);
+//
+// let newArray = usersArr;
+//
+// newArray[2] = 'Mohan';
+//
+// console.log(newArray);
+// console.log(usersArr);
 
 // Array handbook
 
 // Array:   push(), pop(), shift(), unshift(), splice(), slice(),
 // concat(), forEach(), map(), filter(), reduce(), find(), sort()
+
+//Push
+
+let newArr = [1, 2 , 80, 100, 99]
+newArr.push(20)
+
+console.log(newArr)
