abb9bdeb214c2fefe46167c553f4bfca5599b04e
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2024-04-09 20:24:51 +0530

    Initial Commit

--- Day 2/numberMethods.js
+++ Day 2/numberMethods.js
@@ -1,10 +1,14 @@
-let num = 2500;
+let num = 2500500;
 let floAtNum = 256.2240;
 
 //convert to string
 let conString = num.toString();
 
 console.log(typeof conString)
+
+// To local String
+console.log(num.toLocaleString('en-IN')); //convert to local ie INDIAN
+
 
 //Fixed
 
@@ -22,3 +26,5 @@ console.log(parseFloat(809))
 //Presion
 
 console.log(floAtNum.toPrecision(5))
+
+
