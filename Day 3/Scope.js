f0b0622e8ecbff408a33790a1fc6ed68b1746017
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2024-04-11 01:58:37 +0530

    Initial Commit

--- Day 3/Scope.js
+++ Day 3/Scope.js
@@ -11,6 +11,21 @@ let a = 10;
 let b = 20;
 let c = 30;
 
-console.log(a)
-console.log(b)
-console.log(c)
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+function scope() {
+    let sum = 30;
+    let a = 100;
+
+    if(sum === 10){
+        let a = 20;
+        console.log(a)
+    }else
+        sum = a + 20
+    // let a = 9000;
+    return sum;
+}
+
+console.log(scope())
