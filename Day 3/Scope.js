5ef975bb26868cf0ff93f52ba2b37234d107c244
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2024-04-10 23:59:44 +0530

    Initial Commit

--- Day 3/Scope.js
+++ Day 3/Scope.js
@@ -1,6 +1,16 @@
 /*
     There is three types of scope is there is JS
     1. Global Scope
-    2.
-    3.
+    2. Block Scope
+    3. Function Scope
 */
+
+//Global Scope
+
+let a = 10;
+let b = 20;
+let c = 30;
+
+console.log(a)
+console.log(b)
+console.log(c)
