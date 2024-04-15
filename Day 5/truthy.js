27dddef41f1f0cd9cdcf9dc88b291cf9460ab659
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2024-04-15 21:56:19 +0530

    Initial Commit

--- Day 5/truthy.js
+++ Day 5/truthy.js
@@ -2,10 +2,18 @@
 
 
 
-
-
 //ternary operator
 
 let priceOfMobile = 8000;
 
 priceOfMobile <= 9000 ? console.log(`${priceOfMobile},Price is less then 9000`): console.log(`${priceOfMobile},Price is more then 9000.`);
+
+//Nullish coalescing operator usecases
+let holderl;
+
+holderl = 90 ?? undefined ?? NaN;
+
+console.log(holderl)
+
+
+
