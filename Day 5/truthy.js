526ec0c79d5c3b8934a0e80858e441f56abbb21d
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2024-04-15 21:53:54 +0530

    Initial Commit

---
+++ Day 5/truthy.js
@@ -0,0 +1,11 @@
+//Truthy Values.
+
+
+
+
+
+//ternary operator
+
+let priceOfMobile = 8000;
+
+priceOfMobile <= 9000 ? console.log(`${priceOfMobile},Price is less then 9000`): console.log(`${priceOfMobile},Price is more then 9000.`);
