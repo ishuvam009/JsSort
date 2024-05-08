0cd2e9922aa6caa65523efe90159bdedd215e898
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2024-05-08 18:04:58 +0530

    Initial Commit

--- Day 6/loop1.js
+++ Day 6/loop1.js
@@ -5,3 +5,30 @@
 //     console.log(i)
 // }
 
+ // for (let i = 1;i <= 10;i++){
+ //     let element = i;
+ //
+ //     if (element == 5){
+ //         console.log('5 is a great number.')
+ //     }
+ //
+ //     console.log(element)
+ // }
+
+//
+// for(let i = 0;i<=10;i++){
+//     console.log(`Value of outer loop is: ${i}`);
+//     for(let j = 0;j<=10;j++){
+//         console.log(`Value of inner loop is: ${j}`);
+//     }
+// }
+
+
+//1 to 10 multiplication table
+
+for(let i=1;i<=10;i++){
+    for(let j=1;j<=10;j++){
+        console.log(`${i} * ${j} = ${i*j}`);
+    }
+}
+
