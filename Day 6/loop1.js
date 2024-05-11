9f559dff5e5be8a1af48cba86c550d5645de5eee
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2024-05-11 23:55:15 +0530

    Initial Commit

--- Day 6/loop1.js
+++ Day 6/loop1.js
@@ -27,8 +27,11 @@
 //1 to 10 multiplication table
 
 for(let i=1;i<=10;i++){
+    console.log(`Multiplication table for ${i}`)
     for(let j=1;j<=10;j++){
         console.log(`${i} * ${j} = ${i*j}`);
     }
 }
+
+
 
