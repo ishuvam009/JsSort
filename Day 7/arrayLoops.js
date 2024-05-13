ac6d5b0cc6fc7c8d8b50dd99aa8046d57242e045
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2024-05-13 12:26:05 +0530

    Initial Commit

--- Day 7/arrayLoops.js
+++ Day 7/arrayLoops.js
@@ -10,9 +10,37 @@ for (const num of arr) {
 const greet = 'Hello World.'
 
 for (const greetings of greet) {
-    console.log(`each word is: ${greetings}`)
+    //console.log(`each word is: ${greetings}`)
 
     if (greetings === ' '){
     break;
     }
 }
+
+//Map
+
+const newMap = new Map();
+    newMap.set('IN','India');
+    newMap.set('USA','United States Of America');
+    newMap.set('FR','France');
+    newMap.set('USA','United States Of America');
+
+console.log(newMap)
+
+
+for (const  [keys,values] of newMap){
+    console.log(`The Country Code is: ${keys} & Country name is: ${values}`)
+}
+
+
+// NOT WORKING.
+// const myObject = {
+//     'game1': 'NFS',
+//     'game2': 'GTA5',
+//     'game3': 'Spiderman'
+// }
+//
+// for(const [gamesId, gamesName] of myObject){
+//     console.log(`The game ID is: ${gamesId} and game name is: ${gamesId}.`)
+// }
+
